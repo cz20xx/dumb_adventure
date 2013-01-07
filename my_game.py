986c69db1c828a714c24835ab6588761470a3ef1
LEDShark <karnes.cameron@gmail.com>
2013-01-07 12:53:36 -0700

    trying to do inventory

--- my_game.py
+++ my_game.py
@@ -1,14 +1,22 @@
 from sys import exit
 
-def start():
+# LISTS N' SHIT LIKE YOUR INVENTORY I GUESS
+def check_pockets():
+	inventory = ['a knife', 'whiskey', 'an iphone', 'two coffee creamers']
+	for stuff in inventory:
+		print "You've got: %s" % stuff 
+	
+
+
+# BEGINNING OF THE GAME, FOOL
+def start(why):
+	print why
 	print "You wake up, woozy from probably having drunk too much"
 	print "at your going away party."
 	
-	try:
-		next = str(raw_input("> "))
-	except ValueError:
-		dead("WTF does that mean?")
-		
+	
+	next = str(raw_input("> "))
+	
 	if "look" in next:
 		print "You look around the room. It's full of nothing."
 		print "Literally nothing, dude."
@@ -16,7 +24,14 @@ def start():
 		print "Looks like a bedroom, a kitchen and the front door"
 		print "are place you could go.  But idk w/e."
 		living_room()
-		
+	elif "pockets" or "inventory" in next:
+		check_pockets()
+	elif "exit" in next:
+		print "Thanks for playing, jerk!"
+		exit(0)	
+	else:
+		start("Let's try this one more time")
+
 def living_room():
 	
 	next = raw_input("> ")
@@ -40,7 +55,7 @@ def dead(why):
 	print why, "Good job!"
 	exit(0)
 	
-start()
+start("DUMB ADVENTURE v0.1a\n")
 	
 		
 		
